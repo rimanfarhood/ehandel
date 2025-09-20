0c482a62fb8787cf7b6e06f5bd3261484c8aabc7
Riman Farhood <riman.farhood@gmail.com>
2025-09-20 10:43:44 +0200

    Fix typo in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Navbar } from "@/components/navbar";
-import { Footer } from "@/components/Footer";
+import { Footer } from "@/components/footer";
 import "./globals.css";
 
 const geistSans = Geist({
